640c75928877d3184c1c861f28080d2f609614fd
jainvatsal07 <63360724+jainvatsal07@users.noreply.github.com>
2022-09-27 23:20:15 +0530

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -9,8 +9,6 @@ trainer = ListTrainer(bot)
 trainer.train([
 	'Hi',
 	'Hello',
-	'I need roadmap for Competitive Programming',
-	'Just create an account on GFG and start',
 	'I have a query.',
 	'Please elaborate, your concern',
 	'How long it will take to become expert in Coding ?',
